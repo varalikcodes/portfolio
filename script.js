9be69a06bf863b4f2f3f455f013cb627aecd8829
Varalika Varanasi <160519009+varalikcodes@users.noreply.github.com>
2025-03-25 18:51:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "Varalika Varanasi";
+  const typedText = document.getElementById("typed-text");
+  let i = 0;
 
+  function typeLetter() {
+    if (i < text.length) {
+      typedText.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeLetter, 100); // speed
+    }
+  }
+
+  typeLetter();
+});
