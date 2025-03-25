ed5a81faa78d3f9de31d498cd64f38594f1447ec
Varalika Varanasi <160519009+varalikcodes@users.noreply.github.com>
2025-03-25 19:03:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     if (i < text.length) {
       typedText.textContent += text.charAt(i);
       i++;
-      setTimeout(typeLetter, 100); // speed
+      setTimeout(typeLetter, 100);
     }
   }
 
   typeLetter();
+
+  // Theme toggle
+  const themeToggle = document.getElementById("theme-toggle");
+  const root = document.documentElement;
+
+  // Set default to light
+  root.setAttribute('data-theme', 'light');
+
+  themeToggle.addEventListener("click", () => {
+    const current = root.getAttribute("data-theme");
+    root.setAttribute("data-theme", current === "dark" ? "light" : "dark");
+  });
 });
